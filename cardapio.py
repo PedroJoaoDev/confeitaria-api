78927f14cd9329148dbbcca9c419850a39d1f375
joao1062002 <joao1062002@gmail.com>
2025-11-18 23:34:21 +0000

    enviando arquivo

---
+++ cardapio.py
@@ -0,0 +1,13 @@
+
+def buscar_cardapio():
+    itens = [
+        {
+            "id": 1,
+            "nome": "Chocomoça",
+            "descricao": " Bolo de chocolate com ninho ",
+            "preco": 200.99,
+            "foto": ""
+
+        }
+    ]
+    return itens
